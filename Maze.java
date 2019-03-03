237dd7ca49e54156c53aa50925c4d84e18d9a03b
Samson Badlia <sbadlia00@stuy.edu>
2019-03-02 22:02:36 -0500

    Almost finished

--- Maze.java
+++ Maze.java
@@ -19,42 +19,44 @@ public class Maze{
          throw a FileNotFoundException or IllegalStateException
     */
     public Maze(String filename) throws FileNotFoundException{
-
       animate = false;
-      //constructs array using getRows and getCols
-      int rows = getRows(filename);
-      int cols = getCols(filename);
-      maze = new char[rows][cols];
-      //Prints maze into array
-      File file = new File(filename);
-      Scanner s = new Scanner(file);
-      int j = 0;
-      while (s.hasNextLine()){
-        String str = s.nextLine();
-        for (int i = 0; i < cols; i++){
-          maze[j][i] = str.charAt(i);
+      try {
+        File f = new File(filename);
+        Scanner s = new Scanner(f);
+        int r = 0;
+        int c = 0;
+        while (s.hasNextLine()) {
+          r = s.nextLine().length();
+          c++;
         }
-        j++;
+        maze = new char[c][r];
+        s = new Scanner(f);
+        int i = 0;
+        while (s.hasNextLine()) {
+          String line = s.nextLine();
+          for (int j = 0; j < r; j++) {
+            maze[i][j] = line.charAt(j);
+          }
+          i++;
+        }
       }
-      //checks to make sure has both start and end
-      if (!hasEnd() || !hasStart()) throw new IllegalStateException("Missing either start or end");
+      catch (FileNotFoundException e) {
+          System.out.println("File not found" + filename);
+        }
     }
 
-    public String toString(){
+    public String toString() {
       String s = "";
-      for (int i = 0; i < maze.length; i++){
-        for (int j = 0 ;j < maze[0].length; j++){
-          if (j == maze[0].length - 1) s += "\n";
-          s += maze[i][j];
+      for (char[] r : maze) {
+        for (char c : r) {
+          s += c;
         }
+        s += "\n";
       }
       return s;
     }
 
-    /**
-     * Checks if possible has possible end
-     * @return true if maze has end
-     */
+    /*
     public boolean hasEnd(){
       for (int i = 0; i < maze.length; i++){
         for (int x = 0; x < maze[0].length; x++){
@@ -64,10 +66,6 @@ public class Maze{
       return false;
     }
 
-    /**
-     * Checks if maze has a starting "tile"
-     * @return true if maze has a start tile
-     */
     public boolean hasStart(){
       for (int i = 0; i < maze.length; i++){
         for (int x = 0; x < maze[0].length; x++){
@@ -78,13 +76,9 @@ public class Maze{
     }
 
     public int getRows(String f) throws FileNotFoundException{
-      int rows = 0;
       File file = new File(f);
       Scanner s = new Scanner(file);
-      while (s.hasNextLine()){
-        rows++;
-      }
-      return rows;
+      return s.nextLine().length();
     }
 
     public int getCols(String f) throws FileNotFoundException{
@@ -96,6 +90,7 @@ public class Maze{
       }
       return cols;
     }
+    */
 
     private void wait(int millis){
          try {
@@ -120,13 +115,16 @@ public class Maze{
       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
     */
     public int solve(){
-            //find the location of the S.
-
-            //erase the S
-
-            //and start solving at the location of the s.
-            //return solve(???,???);
-            return 0;
+      int r = 0, c = 0;
+      for (int i = 0; i < maze.length; i++){
+        for (int j = 0; j < maze[0].length; j++){
+          if (maze[i][j] == 'S'){
+            r = i;
+            c = j;
+          }
+        }
+      }
+      return solve(r,c);
     }
 
     /*
@@ -143,7 +141,15 @@ public class Maze{
         All visited spots that are part of the solution are changed to '@'
     */
     private int solve(int row, int col){ //you can add more parameters since this is private
-      int count = 0;
+
+      //automatic animation! You are welcome.
+      if(animate){
+          clearTerminal();
+          System.out.println(this);
+          wait(20);
+      }
+
+      int count = 1;
       char empty = ' ';
       char end = 'E';
       //these represent the next character in each direction
@@ -153,6 +159,7 @@ public class Maze{
       char left = maze[row - 1][col];
       //checks if next move is the end
       if (up == end || down == end || left == end || right == end) {
+        maze[row][col] = '@';
         for (int i = 0; i < maze.length; i++){
           for (int j = 0; j < maze[0].length; j++){
             if (maze[i][j] == '@') count++;
@@ -179,11 +186,7 @@ public class Maze{
       }
 
         //automatic animation! You are welcome.
-        if(animate){
-            clearTerminal();
-            System.out.println(this);
-            wait(20);
-        }
+
         return count;
     }
 
